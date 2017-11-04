65573f154d0007a14b15147d4f4f1ef656676461
Andreas <freakwave2@gmx.de>
2017-11-04 16:29:01 +0100

    added missing changes to index.js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+'use strict'
+var Generator = require("./lib/generator")
+	, Server = require("./lib/server")
+	, Utils = require("./lib/utils");
+
+// create a new project
+// projectpath can be relative or absolute
+exports.init = function(projectpath) {
+	try {
+		Generator.init(Utils.normalize_path(projectpath));
+	} catch (ex) {
+		Utils.error(ex, "init");
+		Utils.info("init -h for details");
+	}
 }
+
+// build a project
+// projectpath can be relative or absolute
+exports.build = function(projectpath = ".") {
+	try {
+		Generator.build(Utils.normalize_path(projectpath));
+	} catch (ex) {
+		Utils.error(ex, "build");
+		Utils.info("build -h for details");
+	}
+}
+
+// host a project
+// projectpath can be relative or absolute
+exports.host = function(projectpath = ".") {
+	try {
+		Server.host(Utils.normalize_path(projectpath));
+	} catch (ex) {
+		Utils.error(ex, "host");
+		Utils.info("host -h for details");
+	}
+}
+
